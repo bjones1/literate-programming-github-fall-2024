b7d6b0dc15260962f75b2905765333c8f42a9a59
rs2664 <rs2664@msstate.edu>
2024-10-06 20:40:45 -0500

    Review comments applied.

--- OnBoarding-rs2664/3-Coding with reflection.py
+++ OnBoarding-rs2664/3-Coding with reflection.py
@@ -1,6 +1,6 @@
 # # [On Boarding Game Program](https://www.codingame.com/training)
 #
-# ##    *Problem-Solving Methodology*
+# ## _Problem-Solving Methodology_
 #
 # ### <span style="text-decoration: underline;"><em><strong>Comprehension of the Problem:</strong></em></span>
 #
@@ -9,7 +9,7 @@
 # space. The objective is to determine which enemy is closest, as this
 # represents the most immediate threat.
 #
-# ###       <span style="text-decoration: underline;"><em>Initial Thoughts:</em></span>
+# ### <span style="text-decoration: underline;"><em>Initial Thoughts:</em></span>
 #
 # To clarify the problem, the scenario was conceptualized as occurring within a
 # Euclidean space, where two enemies are positioned at distinct points. The task
@@ -18,13 +18,14 @@
 # considered, wherein the distances of the two enemies are compared, with the
 # enemy having the shorter distance being identified as the immediate threat.
       
-# Prior to coding, a graphical representation of the problem.
-#
-# ### _<span style="text-decoration: underline;">Visualization and Planning <img src="C:\Users\HP\Downloads\Fall 2024\Literate Programing" alt="">:</span>_
 
+
+# ### _<span style="text-decoration: underline;">Visualization and Planning <img src="C:\Users\HP\Downloads\Fall 2024\Literate Programing" alt="">:</span>_
+#
+# Prior to coding, a graphical representation of the problem.
 # <img src="Drawing-1.jpg" alt="" width="625" height="715">
  
-# ### <span style="text-decoration: underline;"><em>Components of the Program:&nbsp;</em></span>        
+# ### <span style="text-decoration: underline;"><em>Components of the Program:&nbsp;</em></span>         
  # 1.  **Input**: The program takes the names and distances of two enemies as
  #     inputs.
  # 2.  **Comparison**: It compares the two distances.
