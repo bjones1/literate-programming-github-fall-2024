fa47cc16c2e5dd5315081bf22a788e6061aafc05
dw2360 <dw2360@msstate.edu>
2024-10-07 23:51:26 -0500

    Update to Flow of headings
    Updated the flow of the headings, added a synopsis for the backstory, and cleaned up indentations

--- thorgame/dw2360/power_of_thor.py
+++ thorgame/dw2360/power_of_thor.py
@@ -1,22 +1,18 @@
-# # Power of Thor Game: [Codinggame.com](https://www.codingame.com/ide/puzzle/power-of-thor-episode-1)
+# #  Power of Thor Game:  [Codinggame.com](https://www.codingame.com/ide/puzzle/power-of-thor-episode-1)
 # ## Imports
 
 import sys
 import math
 
-# ## **Game Objective**
-#
-
-# <mark>JJM702: (Good use of headings and clear topic sentences.Is *approach* the appropriate heading? It seems this is a detailed explanation of the events of the game. The approach should be your approach to solving this probelm and what you will do. This seems to be under *Fixes* which is to me, unclear.)</mark>
-
-# Use the compass directions to move Thor 1 cell in either direction to reach
+# ## <u> **Game Synopsis** </u>
+# > ![Synopsis](thor_synopsis.png)  
+# ### General Rules
+# > Use the compass directions to move Thor 1 cell in either direction to reach
 # the light of power.
 #
-# You lose by Thor running out of energy or running off the map (40 x 18 grid).
+# > You lose by Thor running out of energy or running off the map (40 x 18 grid).
 #
-# ## **Approach**
-# ![Logic](logic.png)
-#
+# ## <u> **Logic** </u>
 # > The conditional statements begin with comparing Thor's initial XY position
 # > to where the light of power's XY position is located.
 #
@@ -42,10 +38,13 @@ import math
 # - _initial_tx_: Thor's starting X position
 # - _initial_ty_: Thor's starting Y position
 #
-# ### **Logic**
-# ![Example](thorgame_example.png)
+# ## <u> **Approach** </u>
+# > ![Logic](logic.png)
 #
-# ## **Solution**
+# ### **East Example**
+# > ![Example](thorgame_example.png)
+#
+# ## <u> **Solution** </u>
 
 # Variables
 light_x, light_y, initial_tx, initial_ty = [int(i) for i in input().split()]
@@ -57,7 +56,6 @@ while True:
     # Write an action using print To debug: print("Debug messages...",
     # file=sys.stderr, flush=True)
 
-# <mark>JJM702: (The logic in the following conditionals is somewhat confusing due to the use of a single `&` operator, which is a bitwise AND. In Python, use `and` for logical AND, or `&` for bitwise operations. Here, you likely meant to use `and`.)</mark>
 
     if initial_ty < light_y and initial_tx < light_x:
         initial_ty = initial_ty + 1
@@ -81,5 +79,4 @@ while True:
         initial_tx = initial_tx - 1
         print("W")
   
-# <mark>JJM702: (Code reads well in editor. For codeChat you could consider using bold, italic, and underline to denote specific aspects clearly. Lack of visual aids hurts understanding.)</mark>
 # <mark>JJM702: (Indentation and spacing in text could be improved. Grammatically it seems fine. Could use more custom description overall to explain your thoughts e.g. relay information about the game quickly and then introduce your problem solving approach and highlight key insights.)</mark>
